b77794339b53ad31a1e112a4154e7395bc2315d1
xuwei-k <6b656e6a69@gmail.com>
2015-02-25 09:04:25 +0900

    add MsgpackUnion equals/hashCode test

--- src/test/scala/msgpack4z/UnionSpec.scala
+++ src/test/scala/msgpack4z/UnionSpec.scala
@@ -1,11 +1,33 @@
 package msgpack4z
 
 import org.scalacheck.{Arbitrary, Prop}
+import scalaz._
 
 abstract class UnionSpec(name: String) extends SpecBase(name + " union") {
 
+  private implicit val unionArb =
+    Arbitrary(UnionArbitrary.unionGen)
+
   property("union") = Prop.secure{
-    checkLaw(MsgpackUnion.codecInstance, Arbitrary(UnionArbitrary.ulongGen))
+    checkLaw(MsgpackUnion.codecInstance, unionArb)
+  }
+
+  property("union equals hashCode") = Prop.forAll{ a: MsgpackUnion =>
+    val M = MsgpackCodec[MsgpackUnion]
+    val bytes = M.toBytes(a, packer())
+    M.unpackAndClose(unpacker(bytes)) match {
+      case \/-(b) =>
+        (a == b) && (a.hashCode == b.hashCode)
+      case -\/(e) =>
+        println(e)
+        false
+    }
+  }
+
+  property("MsgpackLong and MsgpackULong") = Prop.forAll{ a: Long =>
+    val x = MsgpackLong(a)
+    val y = MsgpackULong(new java.math.BigInteger(a.toString))
+    (y == x) && (x == y) && (x.hashCode == y.hashCode)
   }
 
 }
