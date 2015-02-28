909d33dfa88c1581c8b28c5b3e7f8d930fdd94ca
xuwei-k <6b656e6a69@gmail.com>
2015-03-01 02:28:19 +0900

    add some methods for Opt

--- src/main/scala/msgpack4z/Opt.scala
+++ src/main/scala/msgpack4z/Opt.scala
@@ -4,6 +4,33 @@ final class Opt[A <: AnyRef] private[msgpack4z] (val get: A) extends AnyVal {
   def isEmpty: Boolean = get eq null
   def nonEmpty: Boolean = get ne null
   def toOption: Option[A] = Option(get)
+
+  def map[B <: AnyRef](f: A => B): Opt[B] =
+    if(isEmpty) Opt.empty[B]
+    else new Opt(f(get))
+
+  def orNull: A = get
+
+  def exists(f: A => Boolean): Boolean =
+    if(isEmpty) false
+    else f(get)
+
+  def forall(f: A => Boolean): Boolean =
+    if(isEmpty) true
+    else f(get)
+
+  def filter(f: A => Boolean): Opt[A] =
+    if(isEmpty) this
+    else if(f(get)) this
+    else Opt.empty[A]
+
+  def flatMap[B <: AnyRef](f: A => Opt[B]): Opt[B] =
+    if(isEmpty) Opt.empty[B]
+    else f(get)
+
+  def toList: List[A] =
+    if(isEmpty) Nil
+    else (get :: Nil)
 }
 
 object Opt {
