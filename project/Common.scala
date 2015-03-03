16777929ae330c6b77ae1956be3f7ab90e1f9d6e
xuwei-k <6b656e6a69@gmail.com>
2015-03-03 11:21:56 +0900

    Scala 2.11.6

--- project/Common.scala
+++ project/Common.scala
@@ -75,7 +75,7 @@ object Common {
       "-Ywarn-unused-import" ::
       Nil
     ),
-    scalaVersion := "2.11.5",
+    scalaVersion := "2.11.6",
     crossScalaVersions := scalaVersion.value :: Nil,
     scalacOptions in (Compile, doc) ++= {
       val tag = if(isSnapshot.value) gitHash else { "v" + version.value }
