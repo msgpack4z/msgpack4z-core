c86ced5bf43fc65d923b1f8509f1df84ad851c1b
xuwei-k <6b656e6a69@gmail.com>
2015-06-20 15:08:59 +0900

    scalaprops 0.1.9

--- project/Common.scala
+++ project/Common.scala
@@ -27,7 +27,7 @@ object Common {
     scalapropsWithScalazlaws,
     buildInfoSettings
   ).flatten ++ Seq(
-    scalapropsVersion := "0.1.8",
+    scalapropsVersion := "0.1.9",
     resolvers += Opts.resolver.sonatypeReleases,
     ivyScala ~= { _.map(_.copy(overrideScalaVersion = true)) },
     buildInfoKeys := Seq[BuildInfoKey](
