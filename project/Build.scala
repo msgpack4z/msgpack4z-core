fa245244608f55c7805669d4cf3a16ce894c25d9
xuwei-k <6b656e6a69@gmail.com>
2015-07-23 13:37:59 +0900

    "msgpack4z-native" % "0.1.1"

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object build extends Build {
       ("com.github.xuwei-k" %% "zeroapply-scalaz" % "0.1.2" % "provided") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.4" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.1" % "test") ::
-      ("com.github.xuwei-k" %% "msgpack4z-native" % "0.1.0" % "test") ::
+      ("com.github.xuwei-k" %% "msgpack4z-native" % "0.1.1" % "test") ::
       Nil
     )
   ).settings(
