3231054737136db86e9c6de592f4d263005457af
xuwei-k <6b656e6a69@gmail.com>
2016-01-09 06:25:08 +0900

    multi project. add tests with latest msgpack4z-java

--- project/Build.scala
+++ project/Build.scala
@@ -21,7 +21,6 @@ object build extends Build {
       ("com.github.xuwei-k" % "msgpack4z-api" % "0.2.0") ::
       ("org.scalaz" %% "scalaz-core" % Common.ScalazVersion) ::
       ("com.github.xuwei-k" %% "zeroapply-scalaz" % "0.2.0" % "provided") ::
-      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.2.0" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
       ("com.github.xuwei-k" %% "msgpack4z-native" % "0.2.1" % "test") ::
       Nil
@@ -29,5 +28,36 @@ object build extends Build {
   ).settings(
     Sxr.settings
   )
+
+  private[this] lazy val noPublish = Seq(
+    publishLocal := {},
+    publish := {},
+    publishArtifact := false
+  )
+
+  lazy val root = Project("root", file("root")).settings(
+    Common.settings,
+    noPublish
+  ).aggregate(
+    msgpack4z, testJava07, testJavaLatest
+  )
+
+  lazy val testJava07 = Project("testJava07", file("test-java07")).settings(
+    Common.settings,
+    noPublish,
+    libraryDependencies ++= (
+      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.2.0" % "test") ::
+      Nil
+    )
+  ).dependsOn(msgpack4z % "test->test")
+
+  lazy val testJavaLatest = Project("testJavaLatest", file("test-java-latest")).settings(
+    Common.settings,
+    noPublish,
+    libraryDependencies ++= (
+      ("com.github.xuwei-k" % "msgpack4z-java" % "0.3.0" % "test") ::
+      Nil
+    )
+  ).dependsOn(msgpack4z % "test->test")
 
 }
