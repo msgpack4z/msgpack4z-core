900f723ac1df827efa66cda30cbebd6bb6bb16cf
xuwei-k <6b656e6a69@gmail.com>
2017-10-18 10:03:02 +0900

    Scala 2.12.4

--- project/Common.scala
+++ project/Common.scala
@@ -119,7 +119,7 @@ object Common {
       Nil
     ) ::: unusedWarnings,
     scalaVersion := Scala211,
-    crossScalaVersions := Scala211 :: "2.12.3" :: "2.13.0-M2" :: Nil,
+    crossScalaVersions := Scala211 :: "2.12.4" :: "2.13.0-M2" :: Nil,
     scalacOptions in (Compile, doc) ++= {
       val tag = tagOrHash.value
       Seq(
