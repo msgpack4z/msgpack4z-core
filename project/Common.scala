7dcc42869a299dc0e8ec52e244e701a2ff1e6b9e
kenji yoshida <6b656e6a69@gmail.com>
2017-12-21 10:02:17 +0900

    scalaz 7.2.18

--- project/Common.scala
+++ project/Common.scala
@@ -17,7 +17,7 @@ object Common {
     if(isSnapshot.value) gitHash() else tagName.value
   }
 
-  def ScalazVersion = "7.2.17"
+  def ScalazVersion = "7.2.18"
 
   private[this] def Scala211 = "2.11.11"
 
