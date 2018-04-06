a771f4845d2d8b9a737c0e7d1bf5436203ccf906
xuwei-k <6b656e6a69@gmail.com>
2018-04-06 16:23:45 +0900

    scalaz 7.2.21

--- project/Common.scala
+++ project/Common.scala
@@ -17,7 +17,7 @@ object Common {
     if(isSnapshot.value) gitHash() else tagName.value
   }
 
-  def ScalazVersion = "7.2.20"
+  def ScalazVersion = "7.2.21"
 
   private[this] def Scala211 = "2.11.12"
 
