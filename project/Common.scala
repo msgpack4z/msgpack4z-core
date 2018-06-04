e85f9e485a5524a3deab52a750fc142b20059898
xuwei-k <6b656e6a69@gmail.com>
2018-06-04 10:55:52 +0900

    drop Scala 2.13.0-M3

--- project/Common.scala
+++ project/Common.scala
@@ -127,7 +127,7 @@ object Common {
         "-Yno-adapted-args"
     }.toList,
     scalaVersion := Scala211,
-    crossScalaVersions := Scala211 :: "2.12.6" :: "2.13.0-M3" :: "2.13.0-M4" :: Nil,
+    crossScalaVersions := Scala211 :: "2.12.6" :: "2.13.0-M4" :: Nil,
     scalacOptions in (Compile, doc) ++= {
       val tag = tagOrHash.value
       Seq(
