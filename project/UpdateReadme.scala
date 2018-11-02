2bac6fff6870d80e93bc83a163b3707583ae0ca1
xuwei-k <6b656e6a69@gmail.com>
2018-11-02 12:39:36 +0900

    sbt-release 1.0.10

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -31,7 +31,7 @@ object UpdateReadme {
     IO.write(readmeFile, newReadme)
     val git = new Git(extracted get baseDirectory)
     git.add(readme) ! state.log
-    git.commit(message = "update " + readme, sign = false) ! state.log
+    git.commit(message = "update " + readme, sign = false, signOff = false) ! state.log
     scala.sys.process.Process("git diff HEAD^") ! state.log
     state
   }
