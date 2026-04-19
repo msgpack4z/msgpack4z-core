f6b5090f38ae2a680d03bee6ea3bae86a114fc8d
xuwei-k <6b656e6a69@gmail.com>
2026-04-19 13:52:02 +0900

    Update sbt key syntax

--- project/Build.scala
+++ project/Build.scala
@@ -4,5 +4,5 @@ object build {
   val msgpack4zCoreName = "msgpack4z-core"
   val modules = msgpack4zCoreName :: Nil
 
-  val mimaBasis = SettingKey[String]("mimaBasis")
+  val mimaBasis = settingKey[String]("")
 }
